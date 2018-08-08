2dddf201936319d368621a3df59879d6fae1136c
Bread God <vipcube@gmail.com>
2018-08-08 14:43:33 +0800

    Add the PI-Adapter-GridMerge Module.
    Signed-off-by: Bread God <vipcube@gmail.com>

---
+++ PI-Adapter-GridMerge/src/test/java/tw/fondus/fews/adapter/pi/grid/merge/MergeTest.java
@@ -0,0 +1,36 @@
+package tw.fondus.fews.adapter.pi.grid.merge;
+
+import org.junit.Test;
+
+import tw.fondus.fews.adapter.pi.grid.merge.util.RunArguments;
+
+/**
+ * Unit test of Model executable-adapter for running ESRI Grid ASCII merged model.
+ * 
+ * @author Brad Chen
+ *
+ */
+public class MergeTest {
+
+	@Test
+	public void test() {
+		String[] args = new String[]{
+				"-b",
+				"\\Merge",
+				"-e",
+				"GridMerge.exe",
+				"-i",
+				"UNIT01.xml,zonelist.dat,zone_id.asc,UNITTEMP.asc",
+				"-o",
+				"UNITALL.asc,UNITALL.xml,UNITALL????.asc",
+				"-ed",
+				"/Programs/",
+				"-td",
+				"/Temp/"
+				};
+		
+		RunArguments arguments = new RunArguments();
+		new GridMergeExecutable().execute( args, arguments );
+	}
+
+}
