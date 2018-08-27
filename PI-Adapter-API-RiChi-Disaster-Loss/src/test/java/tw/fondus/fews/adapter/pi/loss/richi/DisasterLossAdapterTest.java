a5b323f7453c4f53bb892b71ecc7e1265d2b267a
JackChao1111 <bochao.liang1111@gmail.com>
2018-08-27 18:37:40 +0800

    Add note. Change MapStacksArgument to PiArgument.
    Signed-off-by: JackChao1111 <bochao.liang1111@gmail.com>

--- PI-Adapter-API-RiChi-Disaster-Loss/src/test/java/tw/fondus/fews/adapter/pi/loss/richi/DisasterLossAdapterTest.java
+++ PI-Adapter-API-RiChi-Disaster-Loss/src/test/java/tw/fondus/fews/adapter/pi/loss/richi/DisasterLossAdapterTest.java
@@ -2,20 +2,28 @@ package tw.fondus.fews.adapter.pi.loss.richi;
 
 import org.junit.Test;
 
+import tw.fondus.commons.fews.pi.util.adapter.PiArguments;
 import tw.fondus.commons.fews.pi.util.adapter.PiBasicArguments;
-import tw.fondus.fews.adapter.pi.loss.richi.util.MapStacksArguments;
 
+/**
+ * The unit test of Disaster Loss Adapter.
+ * 
+ * @author Chao
+ *
+ */
 public class DisasterLossAdapterTest {
 	@Test
 	public void test() {
 		String[] args = new String[]{
 				"-b",
-				"/src/test/resources/",
-				"-m",
-				"mapstacks.xml"
+				"src/test/resources/",
+				"-i",
+				"Search.xml",
+				"-o",
+				""
 				};
 		
-		PiBasicArguments arguments = new MapStacksArguments();
+		PiBasicArguments arguments = new PiArguments();
 		new DisasterLossAdapter().execute(args, arguments);
 	}
 }
