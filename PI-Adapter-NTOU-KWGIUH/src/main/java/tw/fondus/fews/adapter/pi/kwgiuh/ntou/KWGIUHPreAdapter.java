ba0b5e8770bd4c9e5bbc6f778612927b9cf6e770
JackChao1111 <bochao.liang1111@gmail.com>
2019-09-10 11:16:32 +0800

    The first version of Pi-Adapter for running NTOU KWGIUH model from Delft-FEWS.
    Signed-off-by: JackChao1111 <bochao.liang1111@gmail.com>

---
+++ PI-Adapter-NTOU-KWGIUH/src/main/java/tw/fondus/fews/adapter/pi/kwgiuh/ntou/KWGIUHPreAdapter.java
@@ -0,0 +1,128 @@
+package tw.fondus.fews.adapter.pi.kwgiuh.ntou;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.util.StringJoiner;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.joda.time.DateTime;
+
+import nl.wldelft.util.timeseries.TimeSeriesArray;
+import nl.wldelft.util.timeseries.TimeSeriesArrays;
+import strman.Strman;
+import tw.fondus.commons.cli.util.Prevalidated;
+import tw.fondus.commons.fews.pi.config.xml.log.LogLevel;
+import tw.fondus.commons.util.string.StringUtils;
+import tw.fondus.fews.adapter.pi.argument.PiBasicArguments;
+import tw.fondus.fews.adapter.pi.cli.PiCommandLineExecute;
+import tw.fondus.fews.adapter.pi.kwgiuh.ntou.argument.PreAdapterArguments;
+import tw.fondus.fews.adapter.pi.log.PiDiagnosticsLogger;
+import tw.fondus.fews.adapter.pi.util.time.TimeLightUtils;
+import tw.fondus.fews.adapter.pi.util.timeseries.TimeSeriesLightUtils;
+
+/**
+ * Model pre-adapter for running NTOU KWGIUH model from Delft-FEWS.
+ * 
+ * @author Chao
+ *
+ */
+public class KWGIUHPreAdapter extends PiCommandLineExecute {
+
+	public static void main( String[] args ) {
+		PreAdapterArguments arguments = new PreAdapterArguments();
+		new KWGIUHPreAdapter().execute( args, arguments );
+	}
+
+	@Override
+	protected void adapterRun( PiBasicArguments arguments, PiDiagnosticsLogger logger, Path basePath, Path inputPath,
+			Path outputPath ) {
+		PreAdapterArguments preAdapterArguments = (PreAdapterArguments) arguments;
+
+		try {
+			logger.log( LogLevel.INFO, "KWGIUHPreAdapter: Starting KWGIUHPreAdapter process." );
+			Path rainfallPath = Prevalidated.checkExists( inputPath.resolve( preAdapterArguments.getInputs().get( 0 ) ),
+					"KWGIUHPreAdapter: Can not find input XML file of rainfall." );
+
+			TimeSeriesArrays rainfallSeriesArrays = TimeSeriesLightUtils.readPiTimeSeries( rainfallPath );
+
+			logger.log( LogLevel.INFO, "KWGIUHPreAdapter: Building the model input from XML file of rainfall." );
+			this.buildRainfallInput( rainfallSeriesArrays.get( 0 ), preAdapterArguments.getArea(),
+					inputPath.resolve( preAdapterArguments.getInputs().get( 1 ) ) );
+
+			logger.log( LogLevel.INFO, "KWGIUHPreAdapter: Building the model input arguments." );
+			this.buildTempInput( inputPath,
+					Strman.append( basePath.toString(), StringUtils.PATH, preAdapterArguments.getExecutableDir() ),
+					preAdapterArguments.getGeomorphicFactor(),
+					String.valueOf( rainfallSeriesArrays.get( 0 ).getHeader().getTimeStep().getStepMillis() / 60000 ),
+					preAdapterArguments.getInputs().get( 1 ), preAdapterArguments.getOutputs().get( 0 ),
+					preAdapterArguments.getNOverlandFlow(), preAdapterArguments.getNChannel(),
+					preAdapterArguments.getWidth() );
+		} catch (OperationNotSupportedException | IOException e) {
+			logger.log( LogLevel.ERROR, "KWGIUHPreAdapter: Reading timeseries or writing file has something wrong." );
+		}
+
+		logger.log( LogLevel.INFO, "KWGIUHPreAdapter: End KWGIUHPreAdapter process." );
+	}
+
+	private void buildRainfallInput( TimeSeriesArray timeSeriesArray, BigDecimal area, Path rainfallInputPath )
+			throws IOException {
+		StringJoiner joiner = new StringJoiner( StringUtils.BREAKLINE );
+		joiner.add( " ☆★☆ Effective rainfall & Direct runoff ☆★☆" );
+		joiner.add( " " );
+		joiner.add( " Dr. Lee, Kwan Tun" );
+		joiner.add( " Fax:**-********" );
+		joiner.add( " Email:ktlee@ntou.edu.tw" );
+		joiner.add( " Watershed Hydrology and Hydraulic Laboratory" );
+		joiner.add( " Department of River and Harbor Engineering" );
+		joiner.add( " National Taiwan Ocean University" );
+		joiner.add( " Keelung, Taiwan 202, R.O.C." );
+		joiner.add( " " );
+		joiner.add( " Rainfall station: -     " );
+		joiner.add( Strman.append( " Interval of rainfall: ",
+				TimeLightUtils.toString( new DateTime( timeSeriesArray.getStartTime() ), "yyyyMMddhh",
+						TimeLightUtils.UTC8 ),
+				StringUtils.HYPHEN, TimeLightUtils.toString( new DateTime( timeSeriesArray.getEndTime() ), "yyyyMMddhh",
+						TimeLightUtils.UTC8 ) ) );
+		joiner.add( " " );
+		joiner.add( Strman.append( StringUtils.SPACE_WHITE, timeSeriesArray.getHeader().getLocationName(),
+				"                         !Name of watershed" ) );
+		joiner.add( Strman.append( "     ", area.setScale( 2, BigDecimal.ROUND_HALF_DOWN ).toString(),
+				"                   !Area (km*km)" ) );
+		joiner.add( Strman.append( "         ", String.valueOf( timeSeriesArray.size() ),
+				"      0	          !Number of rainfall data, number of flow discharge" ) );
+		joiner.add( "       3.00      0.00         !Phi index (mm/hr), Based flow (cms)" );
+		joiner.add( " " );
+
+		StringJoiner valueJoiner = new StringJoiner( StringUtils.TAB );
+		for ( int data = 0; data < timeSeriesArray.size(); data++ ) {
+			valueJoiner.add( String.valueOf( TimeSeriesLightUtils.getValue( timeSeriesArray, data, 0 ) ) );
+		}
+		joiner.add( valueJoiner.toString() );
+
+		try (FileWriter fw = new FileWriter( rainfallInputPath.toFile() ) ) {
+			fw.write( joiner.toString() );
+		}
+	}
+
+	private void buildTempInput( Path inputPath, String workPath, String geomorphicFactor, String timeStep,
+			String rainfallInputPath, String outputPath, BigDecimal nOverlandFlow, BigDecimal nChannel,
+			BigDecimal width ) throws IOException {
+		StringJoiner joiner = new StringJoiner( StringUtils.BREAKLINE );
+		joiner.add( Strman.append( "\'", workPath, geomorphicFactor, "\'	!地文因子檔" ) );
+		joiner.add( Strman.append( timeStep, "  !雨量、流量資料延時(min)" ) );
+		joiner.add( Strman.append( nOverlandFlow.toString(), "  !no" ) );
+		joiner.add( Strman.append( nChannel.toString(), " !nc" ) );
+		joiner.add( Strman.append( width.toString(), "  !B" ) );
+		joiner.add( Strman.append( "\'", workPath, rainfallInputPath, "\'		!雨量檔" ) );
+		joiner.add( Strman.append( "\'", workPath, "\'" ) );
+		joiner.add( "UH.txt		!單位歷線輸出檔案" );
+		joiner.add( Strman.append( outputPath, "	!逕流模擬結果輸出檔案" ) );
+
+		try (FileWriter fw = new FileWriter( inputPath.resolve( "temp.txt" ).toFile() ) ) {
+			fw.write( joiner.toString() );
+		}
+	}
+}
---
+++ PI-Adapter-NTOU-KWGIUH/src/main/java/tw/fondus/fews/adapter/pi/kwgiuh/ntou/argument/PreAdapterArguments.java
@@ -0,0 +1,38 @@
+package tw.fondus.fews.adapter.pi.kwgiuh.ntou.argument;
+
+import java.math.BigDecimal;
+
+import com.beust.jcommander.Parameter;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import tw.fondus.fews.adapter.pi.argument.PiIOArguments;
+
+/**
+ * Model pre-adapter arguments for running NTOU KWGIUH model.
+ * 
+ * @author Chao
+ *
+ */
+
+@Data
+@EqualsAndHashCode( callSuper = false )
+public class PreAdapterArguments extends PiIOArguments {
+	@Parameter( names = { "--edir", "-ed" }, description = "The model executable directory path, relative to the current working directory." )
+	private String executableDir = "Work\\";
+	
+	@Parameter( names = { "--gfactor", "-gf" }, required = true, description = "The file name of geomorphic factor." )
+	private String geomorphicFactor;
+
+	@Parameter( names = { "--area", "-a" }, required = true, description = "The area of catchment(km^2)." )
+	private BigDecimal area;
+	
+	@Parameter( names = { "--noverlandflow", "-no" }, required = true, description = "The argument of Manning N for overland flow." )
+	private BigDecimal nOverlandFlow;
+	
+	@Parameter( names = { "--nchannel", "-nc" }, required = true, description = "The argument of Manning N for river channel." )
+	private BigDecimal nChannel;
+	
+	@Parameter( names = { "--width", "-w" }, required = true, description = "The width of river outflow." )
+	private BigDecimal width;
+}
