7b26d0dfca9c76c18bdebf662762fca891870e70
Bread God <Vipcube@users.noreply.github.com>
2021-05-17 00:04:14 +0800

    Finished adapter.

--- PI-Adapter-GridCorrect-FeatureThreshold/src/main/java/tw/fondus/fews/adapter/pi/grid/correct/GridCorrectFeatureThresholdAdapter.java
+++ PI-Adapter-GridCorrect-FeatureThreshold/src/main/java/tw/fondus/fews/adapter/pi/grid/correct/GridCorrectFeatureThresholdAdapter.java
@@ -3,20 +3,27 @@ package tw.fondus.fews.adapter.pi.grid.correct;
 import com.google.common.base.Preconditions;
 import nl.wldelft.util.timeseries.TimeSeriesArrays;
 import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
 import tw.fondus.commons.cli.util.Prevalidated;
 import tw.fondus.commons.fews.pi.config.xml.log.LogLevel;
 import tw.fondus.commons.fews.pi.util.timeseries.TimeSeriesUtils;
 import tw.fondus.commons.fews.pi.util.transformation.Aggregations;
 import tw.fondus.commons.json.geojson.model.feature.FeatureCollection;
 import tw.fondus.commons.json.util.gson.GsonMapperRuntime;
+import tw.fondus.commons.nc.NetCDFReader;
 import tw.fondus.commons.spatial.model.grid.StandardGrid;
 import tw.fondus.commons.spatial.util.geojson.GeoJsonMapper;
+import tw.fondus.commons.spatial.util.jts.JTSUtils;
+import tw.fondus.commons.spatial.util.nc.NetCDFGridMapper;
 import tw.fondus.commons.util.file.io.PathReader;
+import tw.fondus.commons.util.math.NumberUtils;
+import tw.fondus.commons.util.math.Numbers;
 import tw.fondus.commons.util.string.Strings;
 import tw.fondus.fews.adapter.pi.argument.PiBasicArguments;
 import tw.fondus.fews.adapter.pi.cli.PiCommandLineExecute;
 import tw.fondus.fews.adapter.pi.grid.correct.argument.RunArguments;
 import tw.fondus.fews.adapter.pi.log.PiDiagnosticsLogger;
+import ucar.ma2.InvalidRangeException;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -77,7 +84,19 @@ public class GridCorrectFeatureThresholdAdapter extends PiCommandLineExecute {
 					logger.log( LogLevel.ERROR, "GridCorrectFeatureThresholdAdapter: Input time series period not exceed duration: {}!", duration );
 				} else {
 					Map<String, BigDecimal> aggregationMap = this.aggregationTimeSeries( timeSeriesArrays, duration );
-					// todo
+					try {
+						List<StandardGrid> grids = this.correctGrids( inputNC, geometryMap, aggregationMap,
+								NumberUtils.create( modelArguments.getThreshold() ), modelArguments.getParameter() );
+						if ( grids.isEmpty() ){
+							logger.log( LogLevel.WARN, "GridCorrectFeatureThresholdAdapter: Correct the grids: {} has something wrong!", inputNC.toString() );
+						} else {
+							BigDecimal scale = grids.get( 0 ).getScale();
+							NetCDFGridMapper.toTYXModel( outputPath.resolve( modelArguments.getOutputs().get( 0 ) ),
+									"Corrected", grids, NumberUtils.notEquals( scale, Numbers.ONE ) );
+						}
+					} catch (IOException | InvalidRangeException e) {
+						logger.log( LogLevel.ERROR, "GridCorrectFeatureThresholdAdapter: Read or Write the grids: {} has something wrong!", inputNC.toString() );
+					}
 				}
 			} catch (IOException e) {
 				logger.log( LogLevel.ERROR, "GridCorrectFeatureThresholdAdapter: No time series found in file in the model input files!" );
@@ -119,10 +138,40 @@ public class GridCorrectFeatureThresholdAdapter extends PiCommandLineExecute {
 			) );
 	}
 
-	private List<StandardGrid> thresholdGrids( Path inputNC,
+	/**
+	 * Correct grids by aggregation values and features.
+	 *
+	 * @param inputNC input netcdf
+	 * @param geometryMap map contain id and geometry
+	 * @param aggregationMap map contain id and aggregation value
+	 * @param threshold value of threshold
+	 * @param parameter parameter
+	 * @return corrected grids
+	 * @throws IOException has IO exception
+	 */
+	private List<StandardGrid> correctGrids( Path inputNC,
 			Map<String, Geometry> geometryMap, Map<String, BigDecimal> aggregationMap,
-			int threshold ){
-		// todo
-		return null;
+			BigDecimal threshold, String parameter ) throws IOException {
+		this.getLogger().log( LogLevel.INFO,
+				"GridCorrectFeatureThresholdAdapter: try to correct the grids: {} with features value and threshold: {}.", inputNC.toString(), threshold );
+		try ( NetCDFReader reader = NetCDFReader.read( inputNC ) ){
+			List<StandardGrid> grids = NetCDFGridMapper.fromTYXModel( reader, parameter );
+			return grids.parallelStream()
+				.peek( grid ->
+					aggregationMap.forEach( (id, value) -> {
+						if ( NumberUtils.less( value, threshold ) && geometryMap.containsKey( id ) ){
+							Geometry geometry = geometryMap.get( id );
+							grid.forEachGridIndexed( (index, c) -> {
+								Point point = JTSUtils.point( c );
+								if ( geometry.intersects( point ) ){
+									grid.setValue( BigDecimal.ZERO, index.getCol(), index.getRow() );
+								}
+							},
+							index -> grid.isNotMissing( index.getCol(), index.getRow() ) );
+						}
+					} )
+				)
+				.collect( Collectors.toList() );
+		}
 	}
 }
--- PI-Adapter-GridCorrect-FeatureThreshold/src/test/java/tw/fondus/fews/adapter/pi/grid/correct/GridCorrectFeatureThresholdAdapterTest.java
+++ PI-Adapter-GridCorrect-FeatureThreshold/src/test/java/tw/fondus/fews/adapter/pi/grid/correct/GridCorrectFeatureThresholdAdapterTest.java
@@ -1,9 +1,19 @@
 package tw.fondus.fews.adapter.pi.grid.correct;
 
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import tw.fondus.commons.nc.NetCDFReader;
+import tw.fondus.commons.spatial.model.grid.StandardGrid;
+import tw.fondus.commons.spatial.util.nc.NetCDFGridMapper;
+import tw.fondus.commons.util.file.PathUtils;
+import tw.fondus.commons.util.math.NumberUtils;
+import tw.fondus.commons.util.math.Numbers;
 import tw.fondus.fews.adapter.pi.grid.correct.argument.RunArguments;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class GridCorrectFeatureThresholdAdapterTest {
 	@BeforeAll
@@ -20,7 +30,9 @@ public class GridCorrectFeatureThresholdAdapterTest {
 				"-ts",
 				"40",
 				"-ff",
-				"Towns.geojson"
+				"Towns.geojson",
+				"-p",
+				"depth_below_surface_simulated"
 		};
 
 		RunArguments arguments = RunArguments.instance();
@@ -28,12 +40,16 @@ public class GridCorrectFeatureThresholdAdapterTest {
 	}
 
 	@Test
-	public void test() {
-
+	public void test() throws IOException {
+		try ( NetCDFReader reader = NetCDFReader.read( "src/test/resources/Output/Output.nc" ) ){
+			StandardGrid grid = NetCDFGridMapper.fromSliceTYXModel( reader, "depth_below_surface_simulated", 0 );
+			BigDecimal max = grid.max();
+			Assertions.assertTrue( NumberUtils.equals( max, Numbers.ZERO ) );
+		}
 	}
 
 	@AfterAll
 	public static void after(){
-
+		PathUtils.deleteIfExists( PathUtils.path( "src/test/resources/Output/Output.nc" ) );
 	}
 }
