18621f576334a39872d0440bf2c669ebc92fe386
Bread God <Vipcube@users.noreply.github.com>
2021-11-03 17:19:02 +0800

    Add the workflow state adapter arguments.

---
+++ PI-Adapter-WorkflowState/src/main/java/tw/fondus/fews/adapter/pi/workflow/state/argument/WorkflowStateArguments.java
@@ -0,0 +1,36 @@
+package tw.fondus.fews.adapter.pi.workflow.state.argument;
+
+import com.beust.jcommander.Parameter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import tw.fondus.fews.adapter.pi.argument.PiIOArguments;
+
+import java.util.List;
+
+/**
+ * Model adapter arguments for workflow state with FEWS.
+ *
+ * @author Brad Chen
+ *
+ */
+@Data
+@SuperBuilder
+@ToString( callSuper = true )
+@EqualsAndHashCode( callSuper = true )
+public class WorkflowStateArguments extends PiIOArguments {
+	@Parameter( names = { "--properties" },
+				description = "The properties with comma, and format is key:value.",
+				listConverter = PropertiesConverter.class )
+	private List<PropertiesConverter.KeyValue> properties;
+
+	/**
+	 * Create the argument instance.
+	 *
+	 * @return argument instance
+	 */
+	public static WorkflowStateArguments instance(){
+		return WorkflowStateArguments.builder().build();
+	}
+}
---
+++ PI-Adapter-WorkflowState/src/test/java/tw/fondus/fews/adapter/pi/workflow/state/argument/WorkflowStateArgumentsTest.java
@@ -0,0 +1,33 @@
+package tw.fondus.fews.adapter.pi.workflow.state.argument;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import tw.fondus.commons.cli.util.JCommanderRunner;
+
+/**
+ * The unit test of WorkflowStateArguments.
+ *
+ * @author Brad Chen
+ *
+ */
+public class WorkflowStateArgumentsTest {
+	@Test
+	public void test(){
+		String[] args = {
+				"-b",
+				"src/test/resources",
+				"-i",
+				"",
+				"-o",
+				"",
+				"--properties",
+				"key1:value1,key2:value2,key3:value3"
+		};
+
+		JCommanderRunner.execute( args, WorkflowStateArguments.instance(), "test", arguments -> {
+			Assertions.assertEquals( 3, arguments.getProperties().size() );
+			Assertions.assertEquals( "key2", arguments.getProperties().get( 1 ).getKey() );
+			Assertions.assertEquals( "value2", arguments.getProperties().get( 1 ).getValue() );
+		} );
+	}
+}
